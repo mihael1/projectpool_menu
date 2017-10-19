6d729ee7ce77ffe9f90f1c39f3fb338625374c54
Mihael Šandro <Mihael@desktop-p9m329e.kea.local>
2017-10-19 13:28:22 +0200

    final

--- java.js
+++ java.js
@@ -1,10 +1,14 @@
 let menu = document.querySelector("#topitem");
 let triangles = document.querySelector("#navbar")
 //let javaScript = document.querySelector("#javascript")
+let arrow = document.querySelector ("#arrow")
+let bars = document.querySelector("#bars");
 menu.addEventListener('click', menuUnfolds);
 
 function menuUnfolds() {
     //javaScript.classList.add("js");
+    arrow.classList.toggle("appear");
+    bars.classList.toggle("change");
     console.log("clicked")
     let javaScript = document.querySelector('#js');
     javaScript.classList.toggle("moveFirst");
@@ -17,5 +21,13 @@ function menuUnfolds() {
     let pipePiper = document.querySelector('#pp');
     pipePiper.classList.toggle("movePipe");
 
-
 }
+
+
+/*
+function swapBars(){
+    console.log("bars");
+if (state == 'bar'){
+    myBar.setAttribute("#bars", "")
+}
+*/
